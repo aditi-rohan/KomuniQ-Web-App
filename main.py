21a681b77226b6d14936d77dac093637ae1f5e90
aditi-rohan <124546107+aditi-rohan@users.noreply.github.com>
2023-03-16 02:23:17 +0000

    refined login.html added login.css and other css pages under css folder under static folder added images folder and images under static folder all styling except body{..} from the css file shows up on the page so the body{...} styling needs to remain on login.html

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from views import views
 
+from replit import db
+
+db["key"] = "value"
+
 app = Flask(__name__)
 
 #register blueprint
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host="0.0.0.0")
